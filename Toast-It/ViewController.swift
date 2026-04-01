4f0216d7f69366ed86dff449a5829ae1f2f1c359
jocelynliaoo <jocelyn.l@wustl.edu>
2026-03-31 23:36:28 -0500

    Initial Commit

---
+++ Toast-It/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Toast-It
+//
+//  Created by Jocelyn Liao on 3/31/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
